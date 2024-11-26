5f0e6d2321b1702986451ff9ccdcb813b6af9900
vedantpawar18 <vedantpawar18@gmail.com>
2024-11-26 23:57:27 +0530

    routes setup completed

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+ 
+// import routes 
+const authRoutes = require('./routes/auth');
 
-app.get("/api/register", (req, res) => {
-  res.json({
-    data: "you hit register endpoint",
-  });
-});
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }));
+
+// middlewares
+app.use('/api', authRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
