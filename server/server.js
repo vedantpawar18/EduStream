9d8af36b2a71c39677bcf4b492f82e4f106d002a
vedantpawar18 <vedantpawar18@gmail.com>
2024-11-27 00:10:28 +0530

    db connection implmented

--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
- 
-// import routes 
-const authRoutes = require('./routes/auth');
+
+// db
+mongoose
+  .connect(process.env.DATABASE_CLOUD)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+// import routes
+const authRoutes = require("./routes/auth");
 
 // app middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use(cors({ origin: process.env.CLIENT_URL }));
 
 // middlewares
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
