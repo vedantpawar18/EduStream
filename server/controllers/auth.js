5063a6e2dde2af32e91842de1473ae41b233bae8
vedantpawar18 <vedantpawar18@gmail.com>
2024-12-01 10:48:16 +0530

    registration enhanced and helpers added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,46 +1,41 @@
-// Import the required classes from AWS SDK v3
-const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const { registerEmailParams } = require('../helpers/email');
 
-// Set up the AWS SES client with credentials
 const sesClient = new SESClient({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body;
-     const region= process.env.AWS_REGION  
-    const params = {
-        Source: process.env.EMAIL_FROM,
-        Destination: {
-            ToAddresses: [email]
-        },
-        ReplyToAddresses: [process.env.EMAIL_TO],
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: `<html><body><h1>Hello ${name}</h1 style="color:red;"><p>Test email</p></body></html>`
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Complete your registration'
-            }
-        }
-    };
+  const { name, email, password } = req.body;
 
-    try { 
-        const sendEmailCommand = new SendEmailCommand(params); 
-        const data = await sesClient.send(sendEmailCommand);
-        
-        console.log('Email submitted to SES:', data);
-        res.send('Email sent');
-    } catch (error) {
-        console.log('Error sending email on register:', error);
-        res.send('Email failed');
+  try { 
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
     }
+ 
+    const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, {
+      expiresIn: "10m",
+    }); 
+    const params = registerEmailParams(email, token);  
+    const sendEmailCommand = new SendEmailCommand(params);
+    const data = await sesClient.send(sendEmailCommand); 
+    console.log("Email submitted to SES", data);
+    res.json({
+      message: `Email has been sent to ${email}, Follow the instructions to complete your registration`,
+    });
+  } catch (error) {
+    console.log("Error during registration:", error);
+    res.json({
+      error: `We could not verify your email. Please try again`,
+    });
+  }
 };
