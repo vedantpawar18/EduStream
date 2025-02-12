74a861a691607976204c377d317d8d9730194ff8
vedantpawar18 <vedantpawar18@gmail.com>
2025-02-12 20:32:59 +0530

    user profile update added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -18,7 +18,36 @@ exports.read = async (req, res) => {
 
     res.json({ user, links });
   } catch (err) {
-    console.error(err); 
+    console.error(err);
     res.status(400).json({ error: "Could not fetch user or links" });
+  }
+};
+
+exports.update = async (req, res) => {
+  const { name, password, categories } = req.body;
+
+  if (password && password.length < 6) {
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 6 characters long" });
+  }
+
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { name, password, categories },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(400).json({ error: "Could not find user to update" });
+    }
+
+    updatedUser.hashed_password = undefined;
+    updatedUser.salt = undefined;
+
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ error: "Error updating user" });
   }
 };
